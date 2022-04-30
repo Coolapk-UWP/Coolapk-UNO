37cce5388ec9f5e93bede6f9eb6853a923e9188f
wherewhere <qq2518766683@outlook.com>
2022-04-30 17:29:43 +0800

    新建项目

---
+++ CoolapkUNO/CoolapkUNO/CoolapkUNO.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace CoolapkUNO.Skia.Tizen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new CoolapkUNO.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ CoolapkUNO/CoolapkUNO/CoolapkUNO.Skia.Wpf/Program.cs
@@ -0,0 +1,3 @@
+﻿namespace CoolapkUNO.Skia.Gtk
+{
+}
